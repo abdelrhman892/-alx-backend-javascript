ad2dd6b6035611e33f3618bcb83c02822e7e4313
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-06-27 01:10:11 +0300

    task 1 ✅

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-      if (success){
-          const good = {status: 200, body: 'Success'}
-          resolve(good);
-      }
-      else {
-          reject(Error('The fake API is not working currently'))
-      }
+    if (success) {
+      const good = { status: 200, body: 'Success' };
+      resolve(good);
+    }
+    else {
+      reject(Error('The fake API is not working currently'));
+    }
   });
 }
