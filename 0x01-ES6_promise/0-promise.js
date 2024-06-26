7c145fd142208347a57f60fee20f94c3a0405754
root <abdoayman5m@gmail.com>
2024-06-26 21:57:11 +0000

    task 0 ✅

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise(
-    (resolve, reject) => {
-      const connect = true;
-
-      if (connect) {
-        resolve('Connection established');
-      } else {
-        reject(Error('Connection failed'));
-      }
-    }
-  );
+  return new Promise(() => {});
 }
