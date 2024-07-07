da50187298914df4aa3ef335ce5fd891edb9be31
Abdulrahman <115238439+abdelrhman892@users.noreply.github.com>
2024-07-07 15:36:25 +0300

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,12 +4,7 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const currentYear = getCurrentYear();
-  const budget = {
-    [`income-${currentYear}`]: income,
-    [`gdp-${currentYear}`]: gdp,
-    [`capita-${currentYear}`]: capita,
-  };
-
-  return budget;
+  return{ [`income-${getCurrentYear()}`]: income,
+        [`gdp-${getCurrentYear()}`]: gdp,
+        [`capita-${getCurrentYear()}`]: capita};
 }
